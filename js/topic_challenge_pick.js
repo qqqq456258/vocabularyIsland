55eb7aee39d73fbc8aa781bc3839c090e2b9e4b0
dongen <qqqq456258@gmail.com>
2019-12-26 00:11:36 +0800

    hello

--- js/topic_challenge_pick.js
+++ js/topic_challenge_pick.js
@@ -1,8 +1,11 @@
 $(function () {
-    var level_code = "";
-    var level_code = "";
     
+    /*生成關卡狀態。*/
+    for(let i=1;i<7;i++){
+        $("#slide_0").append("<div id='level_1_"+i+"' class='each_level'><h3 id='title_1_"+i+"' class='title_zone'>Pet & Farm Animals - "+i+"</h3><div class='word_zone'>評分：</div><div id='star_1_"+i+"' class='star_zone'><img class='star' src='material/star_shine.png'><img class='star' src='material/star_shine.png'><img class='star' src='material/star_dark.png'></div></div>");
+    }
     
+    /*轉頁效果。*/
     $("#stages").fullpage({
         resize: true,
         slidesNavigation: true,
@@ -13,17 +16,5 @@ $(function () {
     $(".section.fp-section.active.fp-completely").css("padding-bottom", "30px");
     $(".section.fp-section.active.fp-completely").css("padding-top", "10px");
 
-    
-    
-    $("#slide").append("<div id='level_1_1' class='each_level'>
-                        <h3 id="title_1_1" class="title_zone">Pet & Farm Animals - 1</h3>
-                        <div class="word_zone">評分：</div>
-                        <div id="star_1_1" class="star_zone">
-                            <img class="star" src="material/star_shine.png">
-                            <img class="star" src="material/star_shine.png">
-                            <img class="star" src="material/star_shine.png">
-                        </div>
-                    </div>
-                       ")
     
 });
