37e35affe4ace77f0913675c643337dab4019bbc
qqqq456258 <qqqq456258@gmail.com>
2020-02-22 11:27:07 +0800

    zoom finish

--- js/Animals_island.js
+++ js/Animals_island.js
@@ -1,73 +1,51 @@
-$(function(){
-    let account = "";               // 輸入的帳號。
-    let password = "";              // 輸入的密碼。
-    let name = "Bunny";             // 使用者姓名。
-    let total_building_status = [14,9,12,9]; // 島上每棟建築物全部進度。
-    let building_status = [0,0,0,0];       // 島上每棟建築物實際進度。
-    let width = 1024;
-    let height = 768;
-    var svg = null;
-    var container = null;
-    
+$(function () {
+    let account = ""; // 輸入的帳號。
+    let password = ""; // 輸入的密碼。
+    let name = "Bunny"; // 使用者姓名。
+    let total_building_status = [14, 9, 12, 9]; // 島上每棟建築物全部進度。
+    let building_status = [0, 0, 0, 0]; // 島上每棟建築物實際進度。
+    var svg = d3.select('svg'); //用 Svg 控制整個頁面。
+    var g = d3.select('g'); //將 g element 放入 SVG 中，並包覆著頁面所有的 DOM element，以便控制縮放變化。
+
     /*抓每個島嶼的關卡進度。*/
-    function getStatus(){
+    function getStatus() {
         /* 未完成 */
     }
-    
+
     /*印出島嶼的樣貌。*/
-    function printIsland(){
+    function printIsland() {
         /* 未完成 */
     }
-    
-    /*縮放地圖的功能。*/
+
+    /* d3.js 的功能，設定縮放事件的內容*/
     function zoomed() {
-        x = d3.event.translate[0];
-        y = d3.event.translate[1];
-        s = d3.event.scale;
-        container.attr("transform", "translate(" + d3.event.translate + ") scale(" + d3.event.scale + ")");
+        g.attr("transform", d3.event.transform);
     }
-    
+
+    /* 首先，宣告行為 d3.zoom()，再使用call來呼叫，然後設定zoom的參數，最後執行 zoomed 這個function。*/
+    svg.call(d3.zoom()
+        .translateExtent([[0, 0], [1024, 768]])     // 設定 縮放的範圍，避免拖曳拉出視窗之外。
+        .scaleExtent([1, 8])                        // 設定 縮放的倍率範圍。
+        .on("zoom", zoomed));                       // 執行。
+
+
     /*上一頁*/
-    $('#world').on('click',function(){
+    $('#world').on('click', function () {
         console.log('將回到登入畫面。');
         dialog(0);
     });
-    
-    
-    
+
+
+
+
     /*---------------------------------------------------------------------*/
-    
-    
-    
+
+
+
     getStatus();
     printIsland();
 
-    svg = d3.select('#content')
-        .append('svg:svg')
-        .attr({
-            'width': '100%',
-            'height': '100%'
-        });
 
-    container = svg.append('g'); // g element 其功能能將需要縮放的 svg 圖檔 分在一組。
-    
-    container.append("g").select('#island-name-animal');
-    container.append("g").select('#boat');
-    container.append("g").select('#Animals');
-    container.append("g").select('#island-image');
-    container.append("g").select('#content-image');
-    container.append("g").select('.buliding');
-    
-    
-    
-    var x, y, s;
 
-    var zoom = d3.behavior.zoom()
-        .translate([0, 0])
-        .scaleExtent([1, 10])
-        .scale(1)
-        .on("zoom", zoomed);
-    
-    
-    
+
 });
